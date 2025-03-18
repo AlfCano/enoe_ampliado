ae591261a1042d7244f59b8aa88b6e7f50238227
AlfCano <alfonsociologo@gmail.com>
2025-03-17 22:19:09 -0600

    Adecuación a criterios del SISDAI
    Cambios para seguir las pautas del SISDAI.
    Se agrega el archivo LICENSE y el archivo "session_info.txt".
    Que se creó con el paquete "sessioninfo".
    Específicamente con el comando
    'library("dplyr")
    sessioninfo::session_info() %>% capture.output(file="session_info.txt")'

--- import_enoe_ampliado_svy.R
+++ import_enoe_ampliado_svy.R
@@ -1,21 +1,20 @@
 
 ## Construir el objeto para asistir la importación
-
-
 require("librarian") # Instala el paquete "librarian" si no está previamente instalado y lo carga en la sesión.
-shelf(rio, lookup, dplyr, survey) # Ejecuta la función shelf del paquete librarian. Revisa si los paquetes ya están instalados, si no lo están los instala y los carga en la sesión.
+shelf(rkward, rio, lookup, dplyr, survey) # Ejecuta la función shelf del paquete librarian. Revisa si los paquetes ya están instalados, si no lo están los instala y los carga en la sesión.
+## Establecer el directorio de trabajo
+# setwd() # Se asume que se ha designado el directorio de trabajo.
 
 
 # Requiere crear un objeto para repetir el proceso de maneara reiterativa.
-
 local({
 ##Preparar
 library("rio")
 ## Computar
 url_usr <- "https://github.com/AlfCano"
-ruta <- "enoe_ampliado/raw/main/datos/datos_auxiliares"
-arch <- "caja.RData"
-list <- import(file.path(url_usr,ruta,arch), trust = TRUE)
+ruta    <- "enoe_ampliado/raw/main/datos/datos_auxiliares"
+arch    <- "caja.RData"
+list    <- import(file.path(url_usr,ruta,arch), trust = TRUE)
 ##Asignar
 .GlobalEnv$caja <- list
 })
@@ -37,9 +36,7 @@ data <- rio::import(e, trust=TRUE)
 })
 
 ## Una vez que se ha importado todos los elementos que compodrán el análisis.
-
-#Cambia el nombre de cada archivo por el nombre en la variable nom"año".
-
+#Cambia el nombre de cada archivo por el nombre en la variable nom "año".
 library("lookup")
 names(sdem_coes) <- vlookup(names(sdem_coes),
 caja$import,
@@ -110,8 +107,7 @@ lapply(sdem_coes, nrow)
 # c("p6e","p6f","p6g","p6h","p6i")
 
 
-## Se debe homologar la codificación se debe de las tablas se hace lo siguiente y proceder a la unificación se hace lo siguiente caso por caso:
-
+## Para proceder a la unificación se deben atender discrepancias en las etiquetas de valor de las diferentes tablas.
 
 # Para homologar la codificación en la ENOE 2018 y 2019 con las subsecuentes del 2020 a la feecha en la variable rama_est2.
 local({
@@ -132,15 +128,12 @@ lista <- c("t1_2018.df","t1_2019.df","t1_2020.df")
 ## Computar
 for(e in lista) {
 input <- sdem_coes[[e]][["ur"]]
-# Use as.character() como un formato de datos intermedio, para poder añadir o quitar niveles.
+# Usa "as.character()" como un formato de datos intermedio, para poder añadir o quitar niveles.
 recoded <- as.character (sdem_coes[[e]][["ur"]])
 recoded[input == "Urbano"] <- "Muestra urbana" # "Urbano" se cambia por la etiqueta de valor "Muestra urbana".
 recoded[input == "Rural"] <- "Muestra complemento y rural" # "Rural" se cambia por la etiqueta de valor "Muestra complemento y rural".
+## Asignar el resultado
 .GlobalEnv$sdem_coes[[e]][["ur"]] <- as.factor (recoded)
-## Imprimir el resultado
-rk.header ("Re-codificar datos categóricos", parameters=list("Variable de entrada"="sdem_coes[[e]][[\"ur\"]]",
-	"Variable de salida"="sdem_coes[[e]][[\"ur\"]]",
-	"Número de diferencias después de re-codificar"=sum (sdem_coes[[e]][["ur"]] != sdem_coes[[e]][["ur"]], na.rm=TRUE) + sum (is.na (sdem_coes[[e]][["ur"]]) != is.na (sdem_coes[[e]][["ur"]])))) # El output aparece tres veces debido a que se aplicó a los set de datso 2018, 2029 y 2020.
 }
 })
 
@@ -154,16 +147,13 @@ input <- sdem_coes[[e]][["zona"]]
 # Use as.character() como un formato de datos intermedio, para poder añadir o quitar niveles
 recoded <- as.character (sdem_coes[[e]][["zona"]])
 recoded[input == 'Zona ""A""'] <- "Zona 2 Resto del paí­s" # 'Zona ""A""' se cambia por la etiqueta de valor "Zona 2 Resto del paí­s"
-recoded[input == 'Zona ""B""'] <- "Zona 1 Frontera norte" #'Zona ""B""' se cambia por la etiqueta de valor "Zona 1 Frontera norte"
+recoded[input == 'Zona ""B""'] <- "Zona 1 Frontera norte" # 'Zona ""B""' se cambia por la etiqueta de valor "Zona 1 Frontera norte"
+## Asignar el resultado
 .GlobalEnv$sdem_coes[[e]][["zona"]] <- as.factor (recoded)
-## Imprimir el resultado
-rk.header ("Re-codificar datos categóricos", parameters=list("Variable de entrada"="sdem_coes[[e]][[\"zona\"]]",
-	"Variable de salida"="sdem_coes[[e]][[\"zona\"]]",
-	"Número de diferencias después de re-codificar"=sum (sdem_coes[[e]][["zona"]] != sdem_coes[[e]][["zona"]], na.rm=TRUE) + sum (is.na (sdem_coes[[e]][["zona"]]) != is.na (sdem_coes[[e]][["zona"]]))))
 }
 	})
 
-# En el 1t 2019 Una de las tablas coe/sdem tiene Ciudad de México y/o Distrito federal se reparó a nivel de import desde datos abiertos.
+# En el 1t 2019 Una de las tablas coe/sdem tiene Ciudad de México y/o Distrito federal se reparó a nivel de import desde datos abiertos en la carpeta "datos_limpios".
 
 
 #Para unir las tablas con el paquete "dplyr", se cualrequiere que las variables tengan los mismos nombres, por lo que hay que homologar los nombres de los diseños anteriores al 2021 y actualizarlos los nombres equivalentes actuales, en este caso las variables: "est_d",  "t_loc", "cs_p20_des", "cs_p22_des".
@@ -180,13 +170,13 @@ df[["cs_p23_des"]] <- df[["cs_p22_des"]]
 }
 })
 
-##Con el comando siguiente se importa desde github la tabla completa con etiquetas y sociodemográficos que se utilizará para aplicar los metadatos a la unión de las bases de datos que se descargaron anteriormente.
+## Con el comando siguiente se importa desde github la tabla completa con etiquetas y sociodemográficos que se utilizará para aplicar los metadatos a la unión de las bases de datos que se descargaron anteriormente.
 
 #Se importa el listado de etiquetas de valor para el set de datos-
 sd.cs <- rio::import(caja[["sd_cs"]][["arch"]], trust = TRUE)
 #Actualizar caja con enoe_ampliado
 
-#Convertir variables de factor las variables en la tabla "df.factors" dentro de la lista "caja".
+# Se cohersionan en variables tipo "factor" las variables en marco de datos "df.factors" dentro de la lista "caja".
 local({
 library("dplyr")
 library("lookup")
@@ -198,9 +188,9 @@ r <- caja[["df.factors"]] %>% subset(df == e)
 list_names <- r[["vars"]]#enlista las variables deseadas
 ## Crear Variables de factor
 for (i in list_names)  {
-f[[i]] <- as.factor(f[[i]]) #es el mismo resultado pues las variables son numéricas
+f[[i]] <- as.factor(f[[i]]) # Es el mismo resultado pues las variables son numéricas
 }
-#Asignar etiquetas de valor a factores (niveles)
+# Se asignan etiquetas de valor a factores (niveles)
 for (i in list_names) {
 fa <- sd.cs[[i]]
 v <- f[[i]]
@@ -229,13 +219,11 @@ sc <- t1_2018.df %>%
         bind_rows(t1_2024.df)
 detach(sdem_coes) #Retiramos de la ruta global el contenido de la lista "sdem_coes".
 
-ifelse(
-test= nrow(sc) == 2140686, # Si el objeto sc contiene el número de filas esperado,
-yes = rm(sdem_coes, sd.cs), # Entonces se elimina la lista "sdem_coes" y "sd.cs".
-no = NULL # Si es otro valor devuelve NULL.
-)
+if(nrow(sc) == 2140686) {       # Si el objeto sc contiene el número de filas esperado,
+  rm(sdem_coes, sd.cs)          # Entonces se elimina la lista "sdem_coes" y "sd.cs".
+}
 
-#Recodificar "p4d2" #crear hojas de cálculo con los vectores.
+#Recodificar  la variable "p4d2" #crear hojas de cálculo con los vectores.
 local({
 ## Computar
 input <- sc[["p4d2"]]
@@ -266,34 +254,33 @@ recoded <- as.character (sc[["ent"]])
 recoded[input %in% c("Aguascalientes","Baja California","Baja California Sur","Coahuila","Chihuahua","Durango","Nayarit","Nuevo León","Sinaloa","Sonora","Tamaulipas","Zacatecas")] <- "Norte"
 recoded[input %in% c("Colima","Ciudad de México","Guanajuato","Hidalgo","Jalisco","México","Michoacán","Querétaro","San Luis Potosí")] <- "Centro"
 recoded[input %in% c("Campeche","Chiapas","Guerrero","Oaxaca","Puebla","Quintana Roo","Tabasco","Tlaxcala","Morelos","Veracruz","Yucatán")] <- "Sur-Este"
-.GlobalEnv$sc[["ent.z"]] <- as.factor (recoded)
-## Imprimir el resultado
-rk.header ("Re-codificar datos categóricos", parameters=list("Variable de entrada"="des_1t_18_24[[\"variables\"]][[\"ent\"]]",
-	"Variable de salida"="des_1t_18_24[[\"variables\"]][[\"ent.z\"]]",
-	"Número de diferencias después de re-codificar"=sum (sc[["ent"]] != sc[["ent.z"]], na.rm=TRUE) + sum (is.na (sc[["ent"]]) != is.na (sc[["ent.z"]]))))
-}) #Se ignora: "Error en Ops.factor(sc[["ent"]], sc[["ent.z"]]):  los conjuntos de niveles de los factores son diferentes" debido a que es el resultado esperado.
+## Asignar el resultado
+.GlobalEnv$sc[["ent.z"]] <- as.factor (recoded) # Los conjuntos de niveles de los factores son diferentes.
+})
 
-##Crear una variable numérica para contar totales cuando sea creado el objeto "survey.design".
+## Crear una variable numérica para contar totales cuando sea creado el objeto "survey.design".
 sc[["n"]] <- 1
 
+## Asigna las etiquetas en el formato de RKWard.
 local({
 ## Preparar
 library("lookup")
 ## Computar
-list_nem <- caja[["labels"]][["vars"]] # Para simplificar la notación se copia la variable "nemonico" del data.frame "dic" en la lista "meta.sdem" al objeto "list_nem"
+list_nem <- caja[["labels"]][["vars"]] # Para simplificar la notación se copia la variable "vars" del data.frame "labels" en la lista "caja" al objeto "list_nem".
+dic <- caja[["labels"]] # Asigna la tabla "labels" al objeto "dic".
 for (i in list_nem) {
-rk.set.label(sc[[i]], # Asigna las etiquetas de valor en el objeto "d" a cada variable "i".
-vlookup(i,        # Busca los valores "i" en
-caja$labels,    # la tabla seleccionada "dic",
-"vars",       # en la variable "nemonico" y
-"label"))  # devuelve la cadena correspondiente en la variable "nombre_campo".
-                }
+  rk.set.label(
+    sc[[i]], # Asigna las etiquetas de valor en el objeto "d" a cada variable "i".
+      vlookup(i,    # Busca los valores "i" en
+        dic,  # la tabla seleccionada "dic",
+        "vars",       # en la variable "nemonico" y
+        "label"))     # devuelve la cadena correspondiente en la variable "nombre_campo".
+      }
 ## Asignar el resultado
 .GlobalEnv$sc <- sc
 })
 
-
-##Crear el objeto clase de "diseño de encuesta 2" ("survey.design2")
+## Crear el objeto clase de "diseño de encuesta 2" ("survey.design2")
 
 local({
 library("survey")
@@ -303,8 +290,10 @@ weights = ~fac_tri,
 data = sc, nest=TRUE )
 })
 
-ifelse(
-test= nrow(sc) == nrow(des.1[["variables"]]), # Si el objeto sc contiene el número de filas esperado,
-yes = rm(sc), # Entonces se elimina el objeto "sc".
-no = NULL # Si es otro valor devuelve NULL.
-)
+if(nrow(sc) == nrow(des.1[["variables"]])) {     # Si el objeto sc contiene el número de filas al que se encuentra en el diseño de encuesta:
+ rm(sc, caja)                                    # Entonces se elimina el objeto "sc" y la lista "caja".
+}
+
+
+## Ahora se tiene disponible el objeto des.1 en el área de trabajo que cotiene la muestra de los 1t del 2018 al 2024.
+# Puede proceder a ubicar su directorio de trabajo en la carpeta "/codigo" para realizar los procedimientos en el script "descriptivos_sindicalismo_svy.R" que se pueden consultar en la carpeta "/salida".
